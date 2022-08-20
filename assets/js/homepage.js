9d43f755b05dc623bda199657966d4bbca4a1567
G Codes <graciela.mejia1@yahoo.com>
2022-08-19 17:44:40 -0700

    add click and languages queries

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
   //prevent page from refreshing
@@ -42,6 +43,34 @@ var getUserRepos = function (user) {
       //notice this `catch()` getting chained onto the end of the `.then()` method
       alert("Unable to connect to Github");
     });
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub Usr Not Found");
+    }
+  });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -94,4 +123,5 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 //event listeners to forms
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
